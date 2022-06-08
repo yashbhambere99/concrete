2b36c2a5caab292b38413d5d0e7c83dd63868350
yashbhambere99 <102457787+yashbhambere99@users.noreply.github.com>
2022-06-08 11:50:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -61,27 +61,7 @@ def predict_strength(Cement, Blast_Furnace_Slag, Fly_Ash, Water, Superplasticize
     ptint(prediction)
     return prediction[0]
 
-@st.cache(allow_output_mutation=True)
-def get_base64_of_bin_file(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
 
-def set_jpg_as_page_bg(jpg_file):
-    bin_str = get_base64_of_bin_file(jpg_file)
-    page_bg_img = '''
-    <style>
-    body {
-    background-image: url("data:image/jpg;base64,%s");
-    background-size: cover;
-    }
-    </style>
-    ''' % bin_str
-    
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-    return
-
-set_jpg_as_page_bg('image2.jpg')
 def main():
     st.title("Strength Of Cement")
     #st.image("image1.jpg", width=700)
